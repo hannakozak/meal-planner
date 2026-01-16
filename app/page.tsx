ebc5789bbb1711566121188e7899b4c620b760de
hanna <hannawkozak@gmail.com>
2026-01-16 11:34:55 +0000

    style: set up app fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+import { montserrat, lato } from '@/app/ui/fonts'
+
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1>Mealory</h1>
-        <h2>Organise your meals, your way</h2>
-      </main>
-    </div>
+    <main className="flex min-h-screen flex-col p-6">
+      <h1
+        className={`${montserrat.className} text-4xl font-bold text-gray-800 md:text-3xl md:leading-normal`}
+      >
+        Mealory
+      </h1>
+      <h2 className={`${montserrat.className} text-2xl font-bold`}>
+        Organise your meals, your way <br />
+        Discover, save, and share delicious recipes!
+      </h2>
+      <p className="mt-4 text-lg text-gray-700">
+        <span className={lato.className}>
+          Join our community of cooking enthusiasts — find recipes, save your
+          favorites, and share them with others!
+        </span>
+      </p>
+    </main>
   )
 }
